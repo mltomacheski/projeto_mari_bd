d63c6dda9a3b45e0907aaff895df022baf67ae47
mltomacheski <160253044+mltomacheski@users.noreply.github.com>
2024-08-01 09:33:02 -0300

    insert da tabela porofessor

--- save_script.sql
+++ save_script.sql
@@ -38,5 +38,16 @@ create table grade_curricular(
 	agenda_professor_fk int, foreign key(agenda_professor_fk) references agenda_professor(pk)
 );
 
+insert into professor(nome,disciplina)value("Felipe","Matemática");
+insert into professor(nome,disciplina)value("Leticia","História");
+insert into professor(nome,disciplina)value("Ana","Química");
+insert into professor(nome,disciplina)value("Norberto","Banco de Dados");
+insert into professor(nome,disciplina)value("Jessica","Front-end");
 
+update professor set disciplina = "Matemática" where pk=1;
 
+select * from professor;
+
+insert into agenda_professor(disciplina, horario_aula, aula_dia, agenda_professor_fk)value("História","terceira aula",2,3);
+
+select * from agenda_professor;
